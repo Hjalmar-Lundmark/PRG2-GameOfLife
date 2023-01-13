8ed54829b9a1231e79b217e5c376818cb0447e6d
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2023-01-13 13:38:16 +0100

    first try to view my GoL

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -3,14 +3,16 @@ package Model;
 import Other.GoL;
 import View.*;
 
+import java.util.ArrayList;
+
 public class Model {
     private GoL test;
     private int w;
     private int h;
 
     public Model(int width, int height) {
-        this.w = width;
-        this.h = height;
+        w = width;
+        h = height;
         test = new GoL(width, height);         // idk
     }
 
@@ -41,10 +43,24 @@ public class Model {
     }
 
     public Shape[] getShapes() {
+        /*
         Point[] point = {new Point(15,15)};       //test point, idk how to get my data in
-
-
         return (Shape[]) point;
+         */
+
+        ArrayList<Point> points = new ArrayList<Point>();
+        points.add(new Point(7,8));
+        points.add(new Point(8,9));
+        points.add(new Point(9,7));
+        points.add(new Point(9,8));
+        points.add(new Point(9,9));
+
+        Point[] pixelsToDisplay = new Point[points.size()];
+        points.toArray(pixelsToDisplay);
+
+        return (Shape[]) pixelsToDisplay;
+
+        //return (Shape[]) test.getPoints();
     }
 
 }
--- src/Other/GoL.java
+++ src/Other/GoL.java
@@ -18,19 +18,23 @@ public class GoL {          // The world/board
         cells = new Cell[width*height];
         for (int i = 0; i < width; i++) {
             for (int o = 0; o < height; o++) {      //think this correct?
-                //test = !test;                       // every other one is alive shenanigans
+                test = !test;                       // every other one is alive shenanigans
                 cells[i] = new Cell(test, i, o);
             }
         }
 
         //maybe set alives here, to make a pattern
+        /*
         cells[5+(width*2)].setAlive(true);
         cells[6+(width*2)].setAlive(true);
         cells[7+(width*2)].setAlive(true);
+
+         */
     }
 
     public void checkClose(int i) {
         int neighbors = 0;
+
         if (i+1 <= width*height) {       //first 2 ifs in each are for checking edge cases, lets hope this works
             if (this.cells[i].getY() == this.cells[i+1].getY()) {
                 if (this.cells[i + 1].isAlive()) {
@@ -94,22 +98,19 @@ public class GoL {          // The world/board
     }
 
     public void flipAlive(int i) {
-        // wack
         if ((this.cells[i].getNr() < 2 || this.cells[i].getNr() > 3) && this.cells[i].isAlive()) {
             //om levande, döda dem
             this.cells[i].changeState();
         } else if (!this.cells[i].isAlive() && this.cells[i].getNr() == 3) {
             this.cells[i].changeState();
         }
-
-
     }
 
-    public Point[] getPoints() {
+    public Point[] getPoints() {        //I dont remember what this does
         ArrayList<Point> points = new ArrayList<Point>();
-        for (Cell c : cells) {      // idk
-            if (c.isAlive()) {
-                points.add(new Point(c.getX(), c.getY()));
+        for (/*Cell c : cells*/ int p = 0; p < width*height; p++) {      // idk
+            if (this.cells[p].isAlive()) {
+                points.add(new Point(this.cells[p].getX(), this.cells[p].getY()));
             }
         }
         Point[] pointDisplay = new Point[points.size()];
